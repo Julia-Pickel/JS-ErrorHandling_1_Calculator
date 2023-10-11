5a096c57a99b24a196ad84aed5d1612725c7b06a
Julia Pickel <juliapickel@gmx.de>
2023-10-11 12:27:22 +0200

    task completed

--- js/index.js
+++ js/index.js
@@ -2,21 +2,32 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
+const errorMessage = document.querySelector(".error"); //
 
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => {
-    if (b === 0) {
-      throw new Error("Cannot divide by zero!");
+    try {
+      if (b === 0) {
+        throw new Error("Cannot divide by zero!");
+        console.log("${firstNumber} times ${secondNumber} equals to ${result}");
+      }
+      return a / b;
+      //
+    } catch (error) {
+      errorMessage.textContent = error.message;
+      console.log("Please pass a number rather than 0 as divisor, thank you!");
+      return "Error";
     }
-    return a / b;
+    //
   },
 };
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  errorMessage.textContent = ""; //
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
